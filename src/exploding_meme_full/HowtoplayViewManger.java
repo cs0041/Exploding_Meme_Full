a5c96a528f470551836b55ad3d433a986f0be090
cs0041 <pamzalove20@hotmail.com>
2021-05-02 15:41:16 +0700

    xyz

--- src/exploding_meme_full/HowtoplayViewManger.java
+++ src/exploding_meme_full/HowtoplayViewManger.java
@@ -17,7 +17,7 @@ public class HowtoplayViewManger {
 
     
     
-    private int i,x;
+    private int i,x,y,z;
     
     private static final int GAME_WIDTH =  1920 ;
     private static final int GAME_HIGHT =1080;
